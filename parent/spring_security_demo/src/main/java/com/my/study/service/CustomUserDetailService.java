b1e20f19817d8e589c2df9b6a857ff880ec0becd
tanghuai <1057997598@qq.com>
2021-02-19 15:12:05 +0800

    spring-security权限认证

--- parent/spring_security_demo/src/main/java/com/my/study/service/CustomUserDetailService.java
+++ parent/spring_security_demo/src/main/java/com/my/study/service/CustomUserDetailService.java
@@ -3,8 +3,7 @@ package com.my.study.service;
 import com.my.study.Dao.UserDao;
 import com.my.study.entity.UserDO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,8 +11,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * @author : tanghuai
@@ -23,22 +20,19 @@ import java.util.Collection;
 @Slf4j
 public class CustomUserDetailService implements UserDetailsService {
     @Resource
-    UserDao userDao;
+    private UserDao userDao;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // 查询用户
-        UserDO userByUserLogin = userDao.findUserByUserLogin(s);
+        UserDO userByUserLogin = userDao.findUserByUserLogin(username);
         if (userByUserLogin == null) {
-            log.warn("User:{} not found", s);
-            throw new UsernameNotFoundException("User " + s + " was not found in db");
+            log.warn("User:{} not found", username);
+            throw new UsernameNotFoundException("User " + username + " was not found in db");
             //这里找不到必须抛异常
         }
 
         // 2.设置角色
-        Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(userByUserLogin.getRole());
-        grantedAuthorities.add(grantedAuthority);
-        return new User(s, userByUserLogin.getPassword(), grantedAuthorities);
+        return new User(username, userByUserLogin.getPassword(), AuthorityUtils.createAuthorityList());
     }
 }
