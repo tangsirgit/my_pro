aa547781ade4042e4022b2e997c4ac6b9acb086f
tanghuai <1057997598@qq.com>
2021-02-22 10:34:33 +0800

    spring-security 基于注解的权限控制

--- parent/spring_security_demo/src/main/java/com/my/study/SpringSecurityDemoApplication.java
+++ parent/spring_security_demo/src/main/java/com/my/study/SpringSecurityDemoApplication.java
@@ -2,12 +2,15 @@ package com.my.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * @author : tanghuai
  * @date : 2021/2/18 9:31
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.my.study.*"})
+// 开启security注解扫描,securedEnabled,prePostEnabled
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityDemoApplication.class, args);
--- parent/spring_security_demo/src/main/java/com/my/study/controller/HelloController.java
+++ parent/spring_security_demo/src/main/java/com/my/study/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.my.study.controller;
 
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,37 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+    /**
+     * 基于security注解的配置，角色
+     *
+     * @return
+     */
+    @RequestMapping("/all/read")
+    //@Secured({"ROLE_访客","ROLE_管理员"})
+    @PreAuthorize("hasAnyAuthority('admin:read','guest:read')")
+    public String allRead() {
+        return "所有用户都可以访问";
+    }
+
+    /**
+     * 基于security注解的配置，资源
+     * 相当于配置中的access()方法，写动态表达式
+     *
+     * @return
+     */
+    @RequestMapping("/guest/read")
+    @PreAuthorize("hasAnyAuthority('guest:read')")
+    public String guestRead() {
+        return "访客可以访问";
+    }
+
+    @RequestMapping("/admin/read")
+    @Secured({"ROLE_管理员"})
+    public String adminRead() {
+        return "管理员可以访问";
+    }
+
     @GetMapping("/toLogin")
     public ModelAndView toLogin() {
         ModelAndView modelAndView = new ModelAndView("login");
