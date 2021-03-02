dd2b36bceb8cd77006a6ff440e4e07db74a66082
tanghuai <1057997598@qq.com>
2021-03-02 16:39:29 +0800

    spring-cloud-基础搭建

--- spring_cloud_demo_parent/eureka_server01/src/main/java/com/my/study/SpringCloudApplication.java
+++ spring_cloud_demo_parent/eureka_server01/src/main/java/com/my/study/SpringCloudApplication.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
 /**
- * @author tanghuai
- * @date 2021/3/1
+ * @author : tanghuai
+ * @date : 2021/3/2 15:52
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class SpringCloudApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(SpringCloudApplication.class, args);
     }
 }
